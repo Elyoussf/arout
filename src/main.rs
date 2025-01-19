32171825dbce58db0ac753c59219d7592ae13640
Elyoussf <strangeamazigh1968@gmail.com>
2025-01-19 02:24:15 +0100

    testing Raw mode

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use crossterm::terminal::disable_raw_mode;
 fn main(){
     enable_raw_mode().unwrap();
     for byte in stdin().bytes(){
-        let ch = byte.unwrap() as char;
+        let b = byte.unwrap();
+        let ch = b as char;
+        
+        if ch.is_control(){ // Characters like \0 , \n , \t which are not printable
+            println!("Binary : {0:08b} , ASCII : {0:#03}",b);
+        }else{
+            println!("Binary : {0:08b} , ASCII : {0:#03} , Character : {1:#?}",b,ch);
+        }
+
         if ch == 'q'{
             println!("Quitting");
             disable_raw_mode().unwrap();
